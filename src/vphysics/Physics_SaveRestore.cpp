0d358157d58bfa472af7caabc5de105082831078
Deathreus <csmoore96@yahoo.com>
2020-11-03 21:28:32 -0500

    Create Physics_SaveRestore.cpp

---
+++ src/vphysics/Physics_SaveRestore.cpp
@@ -0,0 +1,20 @@
+#include "cbase.h"
+#include "Physics_Environment.h"
+
+bool CPhysicsEnvironment::Save(const physsaveparams_t &params) {
+	NOT_IMPLEMENTED
+	return false;
+}
+
+void CPhysicsEnvironment::PreRestore(const physprerestoreparams_t &params) {
+	NOT_IMPLEMENTED
+}
+
+bool CPhysicsEnvironment::Restore(const physrestoreparams_t &params) {
+	NOT_IMPLEMENTED
+	return false;
+}
+
+void CPhysicsEnvironment::PostRestore() {
+	NOT_IMPLEMENTED
+}
